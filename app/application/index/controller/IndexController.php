5d4e6ea4b390e1e74ac660593c247af6033ede3e
zangchen1998 <1120649862@qq.com>
2019-01-21 22:11:15 +0800

    首页显示表格

--- app/application/index/controller/IndexController.php
+++ app/application/index/controller/IndexController.php
@@ -1,15 +1,25 @@
 <?php
 namespace app\index\controller;
 use think\Db;
-use think\Request;
+use think\facade\Request;
 use think\Controller;
+use app\common\model\StudentWeekPeriod;
+use app\index\model\Student;
+use app\index\model\Week;
 
 class IndexController extends PassController
 {
     public function index()
     {
+       
+        $weeks = Week::getWeeks();
+       
+        $this->assign('weeks', $weeks);
+        // $students = new Student;
+        // $students = Db::table('Student')->field('name')->limit(20)->select();
         
-        return$this->fetch();
+        // $this->assign('students', $students);
+        return $this->fetch();
     }
 
 
